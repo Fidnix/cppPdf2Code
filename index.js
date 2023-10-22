18076f20306feab670843f14f04f4aca83960fa2
Fidel Moises Apari Sanchez <fidel.moises0@gmail.com>
2023-10-21 21:18:28 -0500

    Adding pretty-errors

--- index.js
+++ index.js
@@ -3,23 +3,22 @@ const colors = require('colors');
 const packageZip = require('./src/zipper');
 const formatter = require('./src/formatter');
 const splitter = require('./src/splitter');
-
-// const fs = require('fs');
-// let pdfBuffer = fs.readFileSync('/home/fidel/Descargas/ejemplo.pdf')
-// codePdfParser(pdfBuffer);
+const PrettyError = require('pretty-error');
 
 module.exports = async function (path, dataBuffer){
     return new Promise((resolve, reject)=>{
         pdf(dataBuffer)
             .then(data=>formatter(data.text))
             .then(data=>splitter(data))
-            .then((data)=> packageZip(path, data))
-            .then((projectName)=>{
+            .then(data=> packageZip(path, data))
+            .then(projectName=>{
                 console.log('Se creó correctamente el zip'.green);
                 resolve(projectName)
             })
             .catch(err=>{
-                console.error(err);
+                const pe = new PrettyError();
+                const renderedError = pe.render(err);
+                console.log(renderedError);
                 reject(err);
             })
     })
